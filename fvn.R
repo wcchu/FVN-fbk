8af6263bbc2e34cc583f554772c4306769e08f7a
Wei-Chun Chu <weichun.chu@gmail.com>
2016-05-12 15:37:42 +0200

    output recommended class & score for this class

--- fvn.R
+++ fvn.R
@@ -1,6 +1,6 @@
 # Input:
 #   dvar = predictor variables of the reference set
-#   dcls = a vector of classes corresponding to dvar
+#   dcls = a vector of classes corresponding to dvar. has to be in type Factor.
 #   dfbk = a vector of feedbacks corresponding to dvar (== T means positive response)
 #   dquery = predictor variables of the query dataset
 #   radius = normalized radius of the neighborhood in variable space
@@ -14,14 +14,14 @@
 # Output: recommended sizes (vector)
 NN <- function(dvar, dcls, dfbk, dquery, radius, min.frac, min.cls, op) {
   # * process training data to create "reference data" for NN alg
-  d <- data.frame(dvar, class = dcls, positive = dfbk)
+  dref <- data.frame(dvar, class = dcls, positive = dfbk)
   nvar <- ncol(dvar) # number of variables
   nref <- nrow(dvar) # number of reference points
   lcls <- unique(dcls) # list of classes
   ncls <- length(lcls) # number of classes
-  u <- c() 
+  u <- c()
   for (icls in 1:ncls) {
-    d_cls <- d %>% filter(class == lcls[icls])
+    d_cls <- dref %>% filter(class == lcls[icls])
     uvar <- c()
     for (ivar in 1:nvar) {
       uvar[ivar] <- sd(d_cls[, ivar])
@@ -36,7 +36,9 @@ NN <- function(dvar, dcls, dfbk, dquery, radius, min.frac, min.cls, op) {
     select(-class)
   # run through queries and recommend size for each query
   nq <- nrow(dquery)
-  drec <- c()
+  rcls <- factor(levels = levels(dcls))
+  rscr <- c()
+  #drec <- c()
   for (iq in 1:nq) {
     q <- dquery[iq, ]
     ss <- vector(length = nref)
@@ -44,10 +46,10 @@ NN <- function(dvar, dcls, dfbk, dquery, radius, min.frac, min.cls, op) {
       ss <- ss + ((dvar[, ivar] - as.numeric(q[ivar])) / dunt[, ivar])^2
     }
     distance <- sqrt(ss)
-    d$dist <- distance
+    dref$dist <- distance
     # collect data points in ball
     dball <-
-      d %>%
+      dref %>%
       filter(dist <= radius) %>%
       select(class, positive)
     n_total = nrow(dball)
@@ -64,9 +66,16 @@ NN <- function(dvar, dcls, dfbk, dquery, radius, min.frac, min.cls, op) {
       ) %>%
       filter(frac >= min.frac, n_cls >= min.cls, n_cls > 1) %>%
       data.frame()
-    if (op == 'rate') {thesize = arrange(sball, neg_rate)$class[1]}
-    if (op == 'popularity') {thesize = arrange(sball, desc(n_pos))$class[1]}
-     drec <- c(drec, thesize)
+    if (op == 'rate') {
+      theclass = arrange(sball, neg_rate)$class[1]
+      thescore = arrange(sball, neg_rate)$neg_rate[1]
+    }
+    if (op == 'popularity') {
+      theclass = arrange(sball, desc(n_pos))$class[1]
+      thescore = arrange(sball, desc(n_pos))$n_pos[1]
+    }
+    rcls[iq] <- theclass
+    rscr[iq] <- thescore
   }
-  return(drec)
+  return(data.frame(cls = rcls, scr = rscr))
 }
