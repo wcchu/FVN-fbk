1af9344df63f9813ae96405178b2583435132d09
wcchu <weichun.chu@gmail.com>
2017-03-18 23:28:33 +0100

    create a test

---
+++ fvnfbk_test.R
@@ -0,0 +1,26 @@
+suppressMessages(library(dplyr))
+suppressMessages(library(dtplyr))
+suppressMessages(library(data.table))
+suppressMessages(require(MASS))
+select <- dplyr::select
+
+set.seed(1)
+
+data <-
+  Cars93 %>%
+  mutate(Man.trans.avail = ifelse(Man.trans.avail == "Yes", TRUE, FALSE)) %>%
+  select(Horsepower, Passengers, DriveTrain, Man.trans.avail) %>%
+  data.table()
+  
+samp <- sample(nrow(data), nrow(data)/3)
+que <- data[samp]
+ref <- data[!samp]
+
+suppressMessages(source("fvnfbk.R"))
+p <- fvnfbk(dvar = ref[, c("Horsepower", "Passengers")],
+            dcls = ref[["DriveTrain"]], 
+            dfbk = ref[["Man.trans.avail"]],
+            dquery = que[, c("Horsepower", "Passengers")],
+            radius = 0.5,
+            keyval = "rate",
+            conf.lev = 0.95)
